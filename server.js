83d7fc15c50d8af1582589c67c6da0bc7956c6ee
Geepesh Agrawal <92394142+Geepesh@users.noreply.github.com>
2023-08-27 21:49:01 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,33 +1,5 @@
 const express = require('express')
 const app = express();
-/*const mongoose = require('mongoose')
-const bp = require('body-parser')
-const db = require('./db/db')
-app.use(bp.urlencoded({
-  extended : false
-}))
-
-app.use(bp.json())
-
-mongoose.connect('mongodb+srv://geepesh_agrawal:geepeshagrawal@cluster0.n8viw.mongodb.net/message-database?retryWrites=true&w=majority',{
-  useNewUrlParser : true,
-  useUnifiedTopology : true
-}).then(()=>{
-  console.log('database connected')
-})
-
-app.post('/',(req,res)=>{
-  let dta = req.body.data;
-  const data = new db({data : dta}) 
-  data.save().then(()=>{
-    console.log('data saved successesfully')
-    res.render('res',{
-      dta : dta
-    })
-  })
-  
-})
-*/
 
 
 app.set('view engine','ejs')
@@ -41,7 +13,7 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(4700,()=>{
     console.log('====================================');
     console.log('started');
     console.log('====================================');
